d8218b01b8bb7a04809798dfad77eb0bc92a692b
João Batista <j.batista.romao@gmail.com>
2020-07-20 08:17:56 -0300

    Initial Commit

---
+++ WeCanDraw/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WeCanDraw
+//
+//  Created by Joao Batista on 20/07/20.
+//  Copyright © 2020 Joao Batista. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
